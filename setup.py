e2e381f4646d4173903845e73968fcbb9d373f28
syedhamidali <hamidsyed37@gmail.com>
2023-04-25 00:13:32 -0400

    remove csv

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=["numpy","pandas","csv"], # Required
+    install_requires=["numpy","pandas"], # Required
 )
